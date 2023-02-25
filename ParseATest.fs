c2d5cf62d288d48db1a433788e1357b5658d653e
mivuorin <mikko.h.vuorinen@gmail.com>
2023-02-25 15:19:57 +0200

    Returning strings

--- ParseATest.fs
+++ ParseATest.fs
@@ -5,27 +5,28 @@ open FsUnit
 
 let parseChar char (stream: string) =
     if stream = "" then
-        (false, "")
+        ("No more input", "")
     else
         let first = stream.[0]
         let rest = stream.[1..]
+
         if first = char then
-            (true, rest)
+            ( $"Found '{char}'", rest)
         else
-            (false, stream)
+            ($"Expecting '{char}'. Got '{first}'", stream)
 
 [<Test>]
 let empty_string () =
-    "" |> parseChar 'A' |> should equal (false, "")
+    "" |> parseChar 'A' |> should equal ("No more input", "")
 
 [<Test>]
-let when_char_is_found_return_true_and_rest_of_the_stream () =
-    "Arest" |> parseChar 'A' |> should equal (true, "rest")
+let when_char_is_found_return_message_and_rest_of_the_stream () =
+    "Arest" |> parseChar 'A' |> should equal ("Found 'A'", "rest")
 
 [<Test>]
 let when_char_is_not_found_return_false_and_unmodified_stream () =
-    "rest" |> parseChar 'A' |> should equal (false, "rest")
+    "rest" |> parseChar 'A' |> should equal ("Expecting 'A'. Got 'r'", "rest")
 
 [<Test>]
 let only_char () =
-    "A" |> parseChar 'A' |> should equal (true, "")
+    "A" |> parseChar 'A' |> should equal ("Found 'A'", "")
